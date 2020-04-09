19e2a2421636db0c67da5cfeb1a71045aaf32d2e
FirmanDwiP <firman.informatics@gmail.com>
2020-04-09 14:51:32 +0700

    customers

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+
+  <form @submit.prevent="save">
+  <input type="hidden" v-model="form.id"> 
+  <input type="text" v-model="form.full_name"> <br>
+  <button type="submit" v-show="!updateSubmit" name="button">Simpan</button>
+  <button type="submit" v-show="updateSubmit" @click="update(form)" name="button">Update</button>
+  </form>
+
+    <h4>List</h4>
+    <ul v-for="customer in customers" :key="customer.id"> 
+    <li>{{ customer.full_name }}
+    <button type="submit" @click="edit(customer)" name="button">Edit</button>
+    <button type="submit" name="button">Delete</button>
+    </li>
+    </ul>
+
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      form:{
+        id:'',
+        full_name:''
+      },
+      customers:[],
+      updateSubmit: false
+    }
+  },
+  methods: {
+    load(){
+      axios.get('http://localhost:3000/customers') 
+      .then((res) =>{ 
+        this.customers = res.data 
+        })
+      .catch(()=>{
+        alert('error load data')
+      })
+    },
+    save(){
+      axios.post('http://localhost:3000/customers', this.form)
+      .then(() =>{ 
+        this.load()
+        this.form.full_name =''
+        alert('saving...') 
+        })
+      .catch((err)=>{
+        alert(err)
+      })
+    },
+    edit(customer){
+      this.updateSubmit = true,
+      this.form.id= customer.id,
+      this.form.full_name = customer.full_name
+    },
+    update(form){
+      axios.put('http://localhost:3000/customers/' + form.id ,{
+        full_name: this.form.full_name
+      })
+      .then(() =>{ 
+        this.load()
+        this.form.full_name =''
+        this.updateSubmit = false
+        alert('updated...') 
+        })
+      .catch(()=>{
+        alert('updated eror...')
+      })
+    }
+  },
+  mounted() {
+    this.load()
+  },
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
